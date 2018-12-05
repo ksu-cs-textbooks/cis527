2b0b04fdb3fbac07e56c4c7641d97fae8f803f92
Russell Feldhausen <russfeldh@gmail.com>
2018-12-05 14:46:44 -0600

    Theme

--- static/js/hugo-learn.js
+++ static/js/hugo-learn.js
@@ -61,8 +61,37 @@ images.each(function(index){
 $(document).ready(function(){
   //russfeld disable top sticky bar - I dislike :)
   //$("#top-bar").sticky({topSpacing:0, zIndex: 1000});
+
 });
 
+
+//russfeld deal with themes
+var updateThemeLink = function(){
+  if($('body').hasClass('dark')){
+    $('#themelink').html('<i class="fas fa-sun"></i><span id="top-github-link-text">Light</span>');
+  }else{
+    $('#themelink').html('<i class="fas fa-moon"></i><span id="top-github-link-text">Dark</span>');
+  }
+}
+
+$(document).ready(function(){
+    var theme = localStorage.getItem('theme');
+    if(theme !== ''){
+        $('body').addClass(theme);
+    }
+    updateThemeLink();
+
+    $('#themelink').click(function () {
+      event.preventDefault();
+      $('body').toggleClass('dark');
+      if($('body').hasClass('dark')){
+          localStorage.setItem('theme', 'dark');
+      }else{
+          localStorage.removeItem('theme');
+      }
+      updateThemeLink();
+    });
+});
 
 jQuery(document).ready(function() {
   // Add link button for every
